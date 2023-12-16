454ac9d084bc1d525a699cf0113b06bf58b77546
Stefan Meza <stefanmeza93@gmail.com>
2023-12-16 23:13:18 +0100

    [WIP] Web server in node

--- 03-tut/server.js
+++ 03-tut/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const path = require("paht");
+const path = require("path");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 
@@ -9,3 +9,82 @@ const EventEmmiter = require("events");
 class Emmiter extends EventEmmiter {}
 
 const myEmmiter = new Emmiter();
+
+const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const data = await fsPromises.readFile(filePath, "utf8");
+    response.statusCode(200, { "Content-Type": contentType });
+    response.end(data);
+  } catch (error) {
+    console.log(error);
+    response.statusCode(500);
+    response.end();
+  }
+};
+
+const server = http.createServer((req, res) => {
+  console.log(res);
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".txt":
+      contentType = "text/html";
+      break;
+    default:
+      contentType = "text/html";
+  }
+
+  let filePath =
+    contentType === "text/html" && req.url === "/"
+      ? path.join(__dirname, "views", "index.html")
+      : contentType === "text/html" && req.url.slice(-1) === "/"
+      ? path.join(__dirname, "views", req.url, "index.html")
+      : contentType === "text/html"
+      ? path.join(__dirname, "views", req.url)
+      : path.join(__dirname, req.url);
+
+  // makes .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case "old-page.html":
+        res.writeHead(301, { Location: "new-page.html" });
+        res.end();
+        break;
+      case "www-page.html":
+        res.writeHead(301, { Location: "/" });
+        res.end();
+        break;
+      default:
+        // serve a 404 response
+        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
+    }
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// nastavi od 25:00 - how to build a web server with node js
