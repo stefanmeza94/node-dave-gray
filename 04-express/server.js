793c1223aad6cf59b501036b59c3c1a625e64d22
Stefan Meza <stefanmeza93@gmail.com>
2023-12-26 14:17:05 +0100

    Add whitelist for specific url; Add CORS;Add custom error middleware; Add error handling for html, json and txt

--- 04-express/server.js
+++ 04-express/server.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
-
-const { logger } = require('./middleware/logEvents');
-
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
+
+const whitelist = ["https://www.yoursite.com", "http://127.0.0.1", "http://localhost:3500"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS!"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+// Cross-Origin-Resource-Sharing
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 // in other wods, form data
@@ -19,52 +33,59 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // "^/$|/index(.html)?" this saying that we accept in this route everything that start and end with / also we can access this route only with typing index without .html, or in other words (.html)? is saying that this part is optional
-app.get('^/$|/index(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, 'new-page.html');
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "new-page.html");
 });
 
 // route handlers
 app.get(
-  '/hello(.html)?',
+  "/hello(.html)?",
   (req, res, next) => {
-    console.log('Attempted to load hello.html');
+    console.log("Attempted to load hello.html");
     next();
   },
   (req, res) => {
-    res.send('Hello world');
+    res.send("Hello world");
   }
 );
 
 const one = (req, res, next) => {
-  console.log('One');
+  console.log("One");
   next();
 };
 const two = (req, res, next) => {
-  console.log('Two');
+  console.log("Two");
   next();
 };
 const three = (req, res, next) => {
-  console.log('Three');
-  res.send('Finished');
+  console.log("Three");
+  res.send("Finished");
 };
 
-app.get('/chain(.html)?', [one, two, three]);
+app.get("/chain(.html)?", [one, two, three]);
 
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(errorHandler);
 
-// nastavi od 14:50 - What is Middleware?
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
